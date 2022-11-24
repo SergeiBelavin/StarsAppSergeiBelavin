1f097fdaa92e6e4a6717e83c681fa67a326d1973
SergeiBelavin <116674469+SergeiBelavin@users.noreply.github.com>
2022-11-24 23:47:57 +0300

    Делаю верную сортировку, присваиваю уникальные числа датам для постройки графика

--- app/src/main/java/com/example/retrofitoff/data/repository/Repository.kt
+++ app/src/main/java/com/example/retrofitoff/data/repository/Repository.kt
@@ -7,6 +7,8 @@ import com.example.retrofitoff.data.entity.RepoUser
 
 import com.example.retrofitoff.data.entity.StarGroup
 import com.example.retrofitoff.data.entity.constructor.ConstructorRepo
+import com.example.retrofitoff.data.entity.constructor.ConstructorStar
+import com.example.retrofitoff.data.entity.constructor.ConstructorUser
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -20,29 +22,27 @@ open class Repository() {
     suspend fun getListRepository(userName: String): List<RepoUser> {
         val allName = ArrayList<String>()
         val responseList = ArrayList<RepoUser>()
+        val responseListConv = ArrayList<ConstructorRepo>()
         var pageNumberUser = 1
-        val constructorRepoList = ArrayList<ConstructorRepo>()
+
         return try {
 
             do {
                 val response: List<RepoUser> =
                     RetrofitInstance.api.getRepoList(userName, pageNumberUser)
 
+
                 response.forEach {
                     allName.add(it.name)
                     Log.d("NAME_ALL_REPO", "$allName")
-                    if (allName.size == MAX_PAGE_SIZE) allName.clear()
+                    val constructorRepo = ConstructorRepo(it.id, it.name, it.allStarsCount, ConstructorUser(it.id, it.name))
+                    responseListConv.add(constructorRepo)
+                    Log.d("RESPONSE_CONV", "$responseListConv")
                 }
-                val group = response.groupBy {
-                    it.name
-                }
-
-                Log.d("GROUPBY", "$group")
-
+                if (responseListConv.size == MAX_PAGE_SIZE) allName.clear()
                 responseList.addAll(response)
                 pageNumberUser++
                 Log.d("PAGE_NUM_REPO", "$pageNumberUser")
-
 
             } while (allName.size == MIN_PAGE_SIZE)
             responseList
@@ -54,10 +54,11 @@ open class Repository() {
     }
 
     suspend fun getStarRepo(userName: String, repoName: String): List<StarGroup> {
-        val allDate = ArrayList<Long>()
-        val pageList = ArrayList<Map.Entry<Date, List<StarGroup>>>()
-        val allDateSort = ArrayList<Map.Entry<Date, List<StarGroup>>>()
-        val starsList = mutableListOf<StarGroup>()
+        val pageList = ArrayList<ConstructorStar>()
+        val groupPageList = ArrayList<StarGroup>()
+        val starsList = ArrayList<StarGroup>()
+        val starsListResponse = ArrayList<StarGroup>()
+        val starsListGroup = ArrayList<StarGroup>()
         var pageNumberStar = 1
 
         return try {
@@ -68,20 +69,58 @@ open class Repository() {
                 Log.d("RESPONSE", "$response")
 
                 val calendar = Calendar.getInstance()
+                val daysAgo = ArrayList<Long>()
+                val daysAgo2 = ArrayList<Date>()
+                val daysResponse = ArrayList<Date>()
+                val daysResponseLong = ArrayList<Int>()
+                val daysResponseLong2 = ArrayList<Int>()
+                val calendar2 = Calendar.getInstance()
+
+                var uniqueDatAgoI = 0
+
+                for (i in 0..13) {
+                    calendar2.add(Calendar.DAY_OF_YEAR, -i)
+                    val dayAgo = calendar.time
+                    Log.d("DAYSIT????", "$dayAgo")
+                    val uniqueDatAgoI = dayAgo.date+31*dayAgo.month/100
+                    daysResponseLong.add(uniqueDatAgoI)
+                    Log.d("DAYS????", "$uniqueDatAgoI")
+                }
+
+                var uniqueDaysResponse = 0
+                response.forEach {
+                    val date = Date(it.starredAt.time)
+                    Log.d("DAYSDATE!!!!", "$date")
+                    uniqueDaysResponse = date.date+31*date.month/100
+                    daysResponseLong2.add(uniqueDaysResponse)
+                    Log.d("DAYS!!!!", "$uniqueDaysResponse")
+                }
+
+
+                Log.d("DAYS_AGO", "")
+
                 calendar.add(Calendar.DAY_OF_YEAR, -14)
                 val daysAgoUnix = calendar.timeInMillis
 
                 Log.d("DAYS_CALENDAR", "$daysAgoUnix")
 
                 val groupBy = response.groupBy {
-                    it.starredAt
+                    it.user.name
+                    if (it.starredAt.time < daysAgoUnix) {
+                        groupPageList.add(it)
+                    }
                 }
-                groupBy.forEach {
-                    val dateGrop = it.key.time
-                    Log.d("GROUP_DAY_RESPONSE", "$dateGrop")
-                    if (it.key.time > daysAgoUnix) pageList.add(it)
-                    Log.d("GROUP_DAY_RESPONSE_IT", "$it")
-                }
+
+
+
+
+                Log.d("GROUP_BY1", "$groupBy")
+                Log.d("GROUP_BY1", "$groupBy")
+
+                Log.d("GROUP1", "$starsListGroup")
+                Log.d("GROUP2", "$starsListResponse")
+
+
                 Log.d("PAGE_LIST_SIZE", "${pageList.size}")
                 if (pageList.size == MAX_PAGE_SIZE) {
                     pageList.clear()
